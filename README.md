b6ce800f23b95a107eafa401f24e401f79b61a56
Adam Bourg <Adam.bourg@connextions.com>
2015-11-05 09:59:36 -0700

    Initalized with a color picker in javascript

---
+++ README.md
@@ -0,0 +1,3 @@
+# code_samples
+
+This repo is for Adam Bourg to provide people examples of work he has done and can do. It's a mixture of different things. 
---
+++ javascript/color_picker/js/main.js
@@ -0,0 +1,32 @@
+var settings = {
+    "red": 255,
+    "green": 255,
+    "blue": 255
+}
+
+$(document).ready(function() {
+    // init the buttons on ready: 
+    $("#colorPickerArea input[type='range']").on("change", function(event){
+        // get the name of the color
+        name = $(this).attr('name');
+        // set RGB value equal to the current input value
+        settings[name] = $(this).val(); 
+
+        renderIndividuals(); 
+        renderAll(); 
+    });
+
+    var renderIndividuals = function(){
+        $("#colorPreviews #red").css("background-color", "rgb(" + settings["red"] + ", 0, 0)");
+        $("#colorPreviews #green").css("background-color", "rgb( 0, " + settings["green"] + ", 0)");
+        $("#colorPreviews #blue").css("background-color", "rgb(0, 0, " + settings["blue"] + ")");
+    };
+
+    var renderAll = function(){
+        $("#colorBox").css("background-color", "rgb(" + 
+            settings["red"] + "," +
+            settings["green"] + "," + 
+            settings["blue"]
+        + ")");
+    };
+});
